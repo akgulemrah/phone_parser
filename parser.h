baf05c99c8c8e802e86c98dc14eb2390c01181f3
Emrah Akgül <45803976+emrhAkgl@users.noreply.github.com>
2024-02-15 22:17:43 +0300

    Update parser.h

--- parser.h
+++ parser.h
@@ -2,6 +2,7 @@
 #define _PHONEPARSER_H_
 
 #include <sys/types.h>
+#include <stdio.h>
 
 
 struct phone_data {
@@ -16,20 +17,20 @@ struct phone_data {
 
 
 struct phone_list {
-	struct 		*phone_data;
+	struct phone_data 	*p_data;
 	
-	int			(*add)(struct phone_list *p_list, const char *name, const char *raw_number_array);
-	void		(*print)(struct phone_list *p_list);
+	int			(*add)(struct phone_list **p_list, const char *name, const char *raw_number_array);
+	void			(*print)(struct phone_list **p_list);
 	int			(*read_json)(struct phone_list *p_list, FILE *json_file);
 	int			(*write_json)(struct phone_list *p_list, FILE *json_file);
-	void		(*free)(struct phone_list *p_list);
+	int			(*free)(struct phone_list **p_list);
 };
 
 
 
-extern unsigned int country_code_size; 	// (+)	55
-extern unsigned int area_code_size;	//	543
-extern unsigned int local_code_size;	// 	3372660
+extern const unsigned int country_code_size; 	// (+)	55
+extern const unsigned int area_code_size;	//	543
+extern const unsigned int local_code_size;	// 	3372660
 
 /*	#	#	#	#	#	#	#	#	#	#	#	#	#
 #													#
@@ -39,7 +40,7 @@ extern unsigned int local_code_size;	// 	3372660
 
 // phone_list nesnesini başlatmak için. liste içindeki 
 // fonksiyon işaretçilerine ilk değerler burada atanır. 
-int init_phone_list(struct phone_list);
+int init_phone_list(struct phone_list **p_list);
 
 //phone_list içindeki phone_data structı için alan tahsis eder.
 struct phone_data *phone_data_calloc(void);
@@ -53,18 +54,15 @@ int extract_phone_number(struct phone_data *p_data, const char *raw_number_array
 /*
 name ve raw_number_array parametrelerini işleyip bağlı lisede bir node oluşturup ona ekleyen bir fonksiyon.
 */
-int add_number(struct phone_list *p_list, const char *name, const char *raw_number_array);
+int add_number(struct phone_list **p_list, const char *name, const char *raw_number_array);
 
 
 
-void print_phone_list(phone_list *p_list);
-int read_from_json(phone_list *p_list, FILE *json_file);
-int write_to_json(phone_list *p_list, FILE *json_file);
+void print_phone_list(struct phone_list **p_list);
+int read_from_json(struct phone_list *p_list, FILE *json_file);
+int write_to_json(struct phone_list *p_list, FILE *json_file);
 
-void free_phone_list(phone_list *p_list);
-void phone_data_free(phone_data *p_data);
-
-
-
+int free_phone_list(struct phone_list **p_list);
+int phone_data_free(struct phone_data **p_data);
 
 #endif
