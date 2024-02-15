a63fe2ce96c8f29f1cb7f703df41ae3d00639de7
Emrah Akgül <45803976+emrhAkgl@users.noreply.github.com>
2024-02-15 22:16:50 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
+#include "parser.h"
 
+const unsigned int country_code_size 	= 2;
+const unsigned int area_code_size 	= 3;
+const unsigned int local_code_size 	= 7;
+
+int main(void)
+{
+	struct phone_list *list;
+	int ret = init_phone_list(&list);
+	if (list == NULL) {
+		printf("işlem başarısız hacı\n");
+		return 1;
+	}
+	list->add(&list, "Emrah", "+905dfv45g3372663");
+	list->add(&list, "Cenk", "+905386d5789sd9f7");
+	list->add(&list, "Ali", "lf90545şd887lkd3667");
+	printf("ret: %d\n", ret);
+
+	list->print(&list);
+
+	list->free(&list);
+	return 0;
+}
