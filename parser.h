5a0b293f4ef61db9a680a4974a87ffa1dd1a3d01
Emrah Akgül <45803976+emrhAkgl@users.noreply.github.com>
2024-02-15 22:35:52 +0300

    Update parser.h

--- parser.h
+++ parser.h
@@ -4,7 +4,6 @@
 #include <sys/types.h>
 #include <stdio.h>
 
-
 struct phone_data {
 	char 		*name;
 	char		*country_code;
@@ -14,7 +13,6 @@ struct phone_data {
 	struct		phone_data *next;
 	struct 		phone_data *prev;
 };
-
 
 struct phone_list {
 	struct phone_data 	*p_data;
@@ -26,42 +24,20 @@ struct phone_list {
 	int			(*free)(struct phone_list **p_list);
 };
 
-
-
 extern const unsigned int country_code_size; 	// (+)	55
 extern const unsigned int area_code_size;	//	543
 extern const unsigned int local_code_size;	// 	3372660
 
-/*	#	#	#	#	#	#	#	#	#	#	#	#	#
-#													#
-#						FUNCTIONS					#
-#													#
-#	#	#	#	#	#	#	#	#	#	#	#	#	*/
-
-// phone_list nesnesini başlatmak için. liste içindeki 
-// fonksiyon işaretçilerine ilk değerler burada atanır. 
-int init_phone_list(struct phone_list **p_list);
-
-//phone_list içindeki phone_data structı için alan tahsis eder.
-struct phone_data *phone_data_calloc(void);
-
-
-// add_number fonksiyonuna parametre geçirilen ham metni parçalayıp 
-// phone_data içindeki ilgili alanlara vermek için hazırlayan ve geriye bu değerlerin toplamı
-// uzunluğunda bir char array işaretçisi gönderen fonksiyon.
-int extract_phone_number(struct phone_data *p_data, const char *raw_number_array);
-
 /*
-name ve raw_number_array parametrelerini işleyip bağlı lisede bir node oluşturup ona ekleyen bir fonksiyon.
-*/
+ * Fnctions
+ */
+int init_phone_list(struct phone_list **p_list);
+struct phone_data *phone_data_calloc(void);
+int extract_phone_number(struct phone_data *p_data, const char *raw_number_array);
 int add_number(struct phone_list **p_list, const char *name, const char *raw_number_array);
-
-
-
 void print_phone_list(struct phone_list **p_list);
 int read_from_json(struct phone_list *p_list, FILE *json_file);
 int write_to_json(struct phone_list *p_list, FILE *json_file);
-
 int free_phone_list(struct phone_list **p_list);
 int phone_data_free(struct phone_data **p_data);
 
