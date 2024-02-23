7dfca3f8f0d7f111ab6f9d890cddcc5c71ca8aea
Emrah Akgül <45803976+emrhAkgl@users.noreply.github.com>
2024-02-23 19:06:56 +0300

    Update parser.h

--- parser.h
+++ parser.h
@@ -5,31 +5,31 @@
 #include <stdio.h>
 
 struct phone_data {
-	char 		*name;
-	char		*country_code;
-	char		*area_code;
-	char		*local_code;
+	char 	*name;
+	char	*country_code;
+	char	*area_code;
+	char	*local_code;
 	
-	struct		phone_data *next;
-	struct 		phone_data *prev;
+	struct	phone_data *next;
+	struct	phone_data *prev;
 };
 
 struct phone_list {
 	struct phone_data 	*p_data;
 	
-	int			(*add)(struct phone_list **p_list, const char *name, const char *raw_number_array);
-	void			(*print)(struct phone_list **p_list);
-	int			(*read_json)(struct phone_list *p_list, FILE *json_file);
-	int			(*write_json)(struct phone_list *p_list, FILE *json_file);
-	int			(*free)(struct phone_list **p_list);
+	int	(*add)(struct phone_list **p_list, const char *name, const char *raw_number_array);
+	void	(*print)(struct phone_list **p_list);
+	int	(*read_json)(struct phone_list *p_list, FILE *json_file);
+	int	(*write_json)(struct phone_list *p_list, FILE *json_file);
+	int	(*free)(struct phone_list **p_list);
 };
 
-extern const unsigned int country_code_size; 	// (+)	55
+extern const unsigned int country_code_size; 	// (+)	90
 extern const unsigned int area_code_size;	//	543
 extern const unsigned int local_code_size;	// 	3372660
 
 /*
- * Fnctions
+ * Functions
  */
 int init_phone_list(struct phone_list **p_list);
 struct phone_data *phone_data_calloc(void);
@@ -41,4 +41,4 @@ int write_to_json(struct phone_list *p_list, FILE *json_file);
 int free_phone_list(struct phone_list **p_list);
 int phone_data_free(struct phone_data **p_data);
 
-#endif
+#endif /* _PHONEPARSER_H_ */
